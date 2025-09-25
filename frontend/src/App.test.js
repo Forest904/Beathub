636f7aa35d421666804b9d42e3d6f0876e18c624
luca <lucapalocco@gmail.com>
2025-09-25 14:22:08 +0200

    removed all testing

--- frontend/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders primary navigation links', () => {
-  render(<App />);
-  expect(screen.getByRole('link', { name: /cd burner/i })).toBeInTheDocument();
-  expect(screen.getByRole('link', { name: /artists/i })).toBeInTheDocument();
-  expect(screen.getByRole('link', { name: /download/i })).toBeInTheDocument();
-});
